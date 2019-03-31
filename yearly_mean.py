88d20c1b8df2916eca6bb30f57c88442e2c77c89
Chi-Ju Wu <48927479+Chijuwu90@users.noreply.github.com>
2019-03-31 17:01:16 +0200

    Update yearly_mean.py

--- yearly_mean.py
+++ yearly_mean.py
@@ -9,16 +9,18 @@ Calculate yearly mean value of a time series
 import pandas
 import numpy
 
-def yearly_mean(time,value):
+def yearly_mean(time, value):
+    '''Calculating yearly mean value'''
     # Format of input time : floating year
     ndays = len(value)
     temp = [0]*ndays
     for i in range(ndays):
         temp[i] = (numpy.int_(time[i]), value[i])
-    # convert to dataframe   
-    df = pandas.DataFrame(temp)   
+    # convert to dataframe
+    df = pandas.DataFrame(temp)
     temp = df.groupby(df[0]).mean()
     # get columns from dataframe
-    x = temp.index.values
-    y = temp.values.reshape(len(x))
-    return x, y
+    yearly_time = temp.index.values
+    yearly_value = temp.values.reshape(len(yearly_time))
+    
+    return yearly_time, yearly_value
